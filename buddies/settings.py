ecdbc717258bfec6f6850481483f96105afc558d
Mix Sirapat <sirapat.jr@gmail.com>
2025-02-20 08:09:53 +0900

    install graphene_django package

--- buddies/settings.py
+++ buddies/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "graphene_django",
 ]
 
 MIDDLEWARE = [
@@ -131,4 +132,8 @@ REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.IsAuthenticated",
     ],
+}
+
+GRAPHENE = {
+    "SCHEMA": "transactions.schema.schema",
 }
