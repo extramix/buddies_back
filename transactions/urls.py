2d19a579a94c50fe29a60f26028822b9bf8a588c
ミック(Sirapat) <sirapat.jr@gmail.com>
2025-03-09 10:12:10 +0900

    update url for account and category

--- transactions/urls.py
+++ transactions/urls.py
@@ -2,11 +2,15 @@ from rest_framework.routers import DefaultRouter
 from transactions.views.transaction import TransactionViewSet
 from transactions.views.user import UserViewSet
 from transactions.views.auth import AuthViewSet
+from transactions.views.account import AccountViewSet
+from transactions.views.category import CategoryViewSet
 
 
 router = DefaultRouter()
 router.register(r"transaction", TransactionViewSet, basename="transaction")
 router.register(r"auth", AuthViewSet, basename="auth")
 router.register(r"user", UserViewSet, basename="user")
+router.register(r"account", AccountViewSet, basename="account")
+router.register(r"category", CategoryViewSet, basename="category")
 
 urlpatterns = router.urls
