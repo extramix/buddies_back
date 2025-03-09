bde2ccfc9ad85647989dbb2518075b81646690c8
ミック(Sirapat) <sirapat.jr@gmail.com>
2025-03-09 10:10:35 +0900

    filter transactions only from current user

--- transactions/views/transaction.py
+++ transactions/views/transaction.py
@@ -1,11 +1,15 @@
 from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
 
 from transactions.models import Transaction
 from transactions.serializers import TransactionSerializer
 
 
 class TransactionViewSet(viewsets.ModelViewSet):
-    queryset = Transaction.objects.all()
     serializer_class = TransactionSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Transaction.objects.filter(account__user=self.request.user)
