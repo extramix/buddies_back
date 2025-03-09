43e6ae337f505f75545ce1111c699f99d7560b9e
ミック(Sirapat) <sirapat.jr@gmail.com>
2025-03-09 11:12:26 +0900

    Revert "add validation to make sure current user can't modify another user's data"
    This reverts commit cf059702fdd75cdad60395fbf28807c7b71ea7c5.

--- transactions/views/transaction.py
+++ transactions/views/transaction.py
@@ -1,12 +1,13 @@
 from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework import status
-from transactions.models import Transaction
-from transactions.serializers import TransactionSerializer
 from rest_framework.permissions import IsAuthenticated
 
+from transactions.models import Transaction
+from transactions.serializers import TransactionSerializer
+
+
 class TransactionViewSet(viewsets.ModelViewSet):
-    queryset = Transaction.objects.all()
     serializer_class = TransactionSerializer
     permission_classes = [IsAuthenticated]
 
